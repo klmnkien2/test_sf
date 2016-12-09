39dd7d57d8fe6198b65e630c2f5111ee50a853d3
unknown <kien.dv@altplus.com.vn>
2016-12-09 15:21:19 +0700

    git status

---
+++ src/Gao/AdminBundle/Controller/SecurityController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Gao\AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+
+class SecurityController extends Controller
+{
+    public function loginAction(Request $request)
+    {
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContextInterface::AUTHENTICATION_ERROR
+            );
+        } elseif (null !== $session && $session->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContextInterface::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+        $lastUsername = (null === $session) ? '' : $session->get(SecurityContextInterface::LAST_USERNAME);
+
+        return $this->render(
+            'GaoAdminBundle:Security:login.html.twig',
+            array(
+                'last_username' => $lastUsername,
+                'error' => $error,
+            )
+        );
+    }
+
+    public function forgotAction(Request $request)
+    {
+        $session = $request->getSession();
+
+        $post = Request::createFromGlobals();
+        if ($post->request->has('submit')) {
+            if (!$this->get('form.csrf_provider')->isCsrfTokenValid('forgot_password', $post->request->get('csrf_token'))) {
+                var_dump("token wrong!");die;
+            }
+            $email = $post->request->get('email');
+            $username = $post->request->get('username');
+
+            $session->getFlashBag()->add('success', 'He thong da gui thong tin cap nhat tai khoan cho ban. Vui long kiem tra email cua ban.');
+
+            //$messageTemplate = $this->getParameter('email_forgot_message');
+            //$message = sprintf($messageTemplate, $email, $username);
+        }
+
+        return $this->render(
+            'GaoAdminBundle:Security:forgot.html.twig'
+        );
+    }
+}
---
+++ src/Gao/AdminBundle/Entity/Admin.php
@@ -0,0 +1,449 @@
+<?php
+
+namespace Gao\AdminBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
+
+/**
+ * Admin
+ *
+ * @ORM\Entity(repositoryClass="Gao\AdminBundle\Entity\AdminRepository")
+ * @ORM\Table(name="admin")
+ * @ORM\Entity
+ */
+class Admin implements AdvancedUserInterface, \Serializable
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="creator_id", type="integer", nullable=true)
+     */
+    private $creatorId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="full_name", type="string", length=255, nullable=true)
+     */
+    private $fullName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="phone", type="string", length=20, nullable=true)
+     */
+    private $phone;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, nullable=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=50, nullable=false)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=1000, nullable=false)
+     */
+    private $password;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="role", type="string", length=100, nullable=true)
+     */
+    private $role;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="salt", type="string", length=100, nullable=false)
+     */
+    private $salt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="last_login", type="datetime", nullable=true)
+     */
+    private $lastLogin;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="current_login", type="datetime", nullable=true)
+     */
+    private $currentLogin;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="email_verified", type="boolean", nullable=true)
+     */
+    private $emailVerified;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="blocked", type="boolean", nullable=true)
+     */
+    private $blocked;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set creatorId
+     *
+     * @param integer $creatorId
+     * @return Admin
+     */
+    public function setCreatorId($creatorId)
+    {
+        $this->creatorId = $creatorId;
+
+        return $this;
+    }
+
+    /**
+     * Get creatorId
+     *
+     * @return integer 
+     */
+    public function getCreatorId()
+    {
+        return $this->creatorId;
+    }
+
+    /**
+     * Set fullName
+     *
+     * @param string $fullName
+     * @return Admin
+     */
+    public function setFullName($fullName)
+    {
+        $this->fullName = $fullName;
+
+        return $this;
+    }
+
+    /**
+     * Get fullName
+     *
+     * @return string 
+     */
+    public function getFullName()
+    {
+        return $this->fullName;
+    }
+
+    /**
+     * Set phone
+     *
+     * @param string $phone
+     * @return Admin
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    /**
+     * Get phone
+     *
+     * @return string 
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Admin
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     * @return Admin
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string 
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return Admin
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set role
+     *
+     * @param string $role
+     * @return Admin
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    /**
+     * Get role
+     *
+     * @return string 
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * Set salt
+     *
+     * @param string $salt
+     * @return Admin
+     */
+    public function setSalt($salt)
+    {
+        $this->salt = $salt;
+
+        return $this;
+    }
+
+    /**
+     * Get salt
+     *
+     * @return string 
+     */
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+    /**
+     * Set lastLogin
+     *
+     * @param \DateTime $lastLogin
+     * @return Admin
+     */
+    public function setLastLogin($lastLogin)
+    {
+        $this->lastLogin = $lastLogin;
+
+        return $this;
+    }
+
+    /**
+     * Get lastLogin
+     *
+     * @return \DateTime 
+     */
+    public function getLastLogin()
+    {
+        return $this->lastLogin;
+    }
+
+    /**
+     * Set currentLogin
+     *
+     * @param \DateTime $currentLogin
+     * @return Admin
+     */
+    public function setCurrentLogin($currentLogin)
+    {
+        $this->currentLogin = $currentLogin;
+
+        return $this;
+    }
+
+    /**
+     * Get currentLogin
+     *
+     * @return \DateTime 
+     */
+    public function getCurrentLogin()
+    {
+        return $this->currentLogin;
+    }
+
+    /**
+     * Set emailVerified
+     *
+     * @param boolean $emailVerified
+     * @return Admin
+     */
+    public function setEmailVerified($emailVerified)
+    {
+        $this->emailVerified = $emailVerified;
+
+        return $this;
+    }
+
+    /**
+     * Get emailVerified
+     *
+     * @return boolean 
+     */
+    public function getEmailVerified()
+    {
+        return $this->emailVerified;
+    }
+
+    /**
+     * Set blocked
+     *
+     * @param boolean $blocked
+     * @return Admin
+     */
+    public function setBlocked($blocked)
+    {
+        $this->blocked = $blocked;
+
+        return $this;
+    }
+
+    /**
+     * Get blocked
+     *
+     * @return boolean 
+     */
+    public function getBlocked()
+    {
+        return $this->blocked;
+    }
+
+    /**
+     * Implement security function
+     */
+    public function getRoles()
+    {
+        return array('ROLE_ADMIN');
+    }
+    
+    public function eraseCredentials()
+    {
+    }
+    
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->username,
+            $this->emailVerified,
+            $this->blocked
+        ));
+    }
+    
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            $this->username,
+            $this->emailVerified,
+            $this->blocked
+        ) = unserialize($serialized);
+    }
+    
+    public function isAccountNonExpired()
+    {
+        return true;
+    }
+    
+    public function isAccountNonLocked()
+    {
+        return !($this->blocked);
+    }
+    
+    public function isCredentialsNonExpired()
+    {
+        return true;
+    }
+    
+    public function isEnabled()
+    {
+        return $this->emailVerified;
+    }
+}
